e386dfb1a16683cee941943653fcbd538c75b396
hackibosy <hackibosy@gmail.com>
2023-07-17 23:36:06 +0300

    Creating viewset functionnal for site for candidate

---
+++ candidates/management/commands/loadsitecandidate.py
@@ -0,0 +1,25 @@
+from django.core.management.base import BaseCommand
+from tqdm import tqdm
+from candidates.models import Candidate, Site
+from faker import Faker
+import random
+from mixins.routines import do_or_empty
+from django.contrib.auth.models import User
+
+class Command(BaseCommand):
+    def clean_db(self):
+        for candidate in tqdm(Candidate.objects.all(), 'Clean all sites for all candidates'):
+            for site in candidate.user.site_set.all():
+                site.delete()
+                        
+    def handle(self, *args, **kwargs):
+        self.clean_db()
+        faker = Faker()
+    for candidate in tqdm(Candidate.objects.filter(user__in = User.objects.filter(is_staff=False, is_active=True)), "Creating new sites for candidates."):
+            for _ in range(random.randrange(10)):
+                Site.objects.create(
+                    owner = candidate.user,
+                    title = faker.text(20),
+                    description = do_or_empty(faker.text,300),
+                    link = faker.url()
+                )
--- candidates/serializers/site_serializers.py
+++ candidates/serializers/site_serializers.py
@@ -1,3 +1,4 @@
+from mixins.user_serializer import UserSerializer
 from rest_framework import serializers
 from candidates.models import Site
 
@@ -5,3 +6,11 @@ class SiteListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Site
         fields = 'id', 'title', 'link'
+
+class SiteDetailCandidateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Site
+        fields = 'id', 'title', 'link', 'description', 'owner'
+        extra_kwargs = {
+            'owner': {'write_only': True},
+        }
