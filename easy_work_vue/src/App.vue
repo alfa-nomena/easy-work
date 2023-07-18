68a3e3ff2a78d7a1ca945373a2b5eb0e4a84b851
Nomena Alfa ANDRIAMIFIDISOA <perf.alfa@mmv.madamvalue.mg>
2023-07-18 17:57:18 +0300

    Creating frontend for the API

---
+++ easy_work_vue/src/App.vue
@@ -0,0 +1,48 @@
+<script setup>
+  import HeaderComponent from './components/HeaderComponent.vue'
+</script>
+<template>
+  <HeaderComponent/>
+</template>
+
+<style lang="scss">
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
+<script>
+  export default {
+    name: 'App',
+    components: [HeaderComponent],
+    data(){
+      return {
+        candidates: []
+      }
+    },
+    mounted() {
+      fetch('http://127.0.0.1:8000/api/candidates')
+      .then(res=>res.json())
+      .then(data => {
+        this.candidates = data
+      })
+    }
+  }
+</script>
---
+++ easy_work_vue/src/components/HeaderComponent.vue
@@ -0,0 +1,94 @@
+<template>
+  <nav>
+    <div>
+      <a id="logo" href="index.html">
+        <img src="../assets/logo-header.png" alt="">
+      </a>
+    </div>
+    <ul class="nav nav-tabs">
+      <li v-for="(nav,i) in navs" :key="i" class="nav-item" @click.self="click_nav_button(nav)">
+        <a :href="nav.url" class="nav-link" :class="{'nav-link':nav.is_active}">{{ nav.display_text }}</a> 
+      </li>
+    </ul> 
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'HeaderComponent',
+  data(){
+    return {
+      navs: [
+        {
+          url: '#',
+          is_active: true,
+          display_text: 'Home',
+        },
+        {
+          url: '#',
+          is_active: false,
+          display_text: 'Companies',
+        },
+        {
+          url: '#',
+          is_active: false,
+          display_text: 'Candidates',
+        },
+        {
+          url: '#',
+          is_active: false,
+          display_text: 'Jobs',
+        }
+        ,
+        {
+          url: '#',
+          is_active: false,
+          display_text: 'Login',
+        }
+      ]
+    }
+  },
+  methods: {
+    click_nav_button(el){
+      this.navs.forEach(nav=>{
+        nav.is_active=false
+      })
+      el.is_active=true
+      console.log(el)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+nav{
+  height: 80px;
+  padding: 10px;
+  display: flex;
+  align-items: flex-start;
+  flex-direction: row;
+  justify-content: space-between;
+  border-bottom: 1px solid #6b797c ;
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #6b797c;
+}
+.nav-link:hover{
+  color: #da0833
+}
+.nav-tabs{
+  border: 0;
+}
+</style>
