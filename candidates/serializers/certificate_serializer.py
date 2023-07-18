cbf89ec957335df42a5ac0c668d958d185ee96fc
Nomena Alfa ANDRIAMIFIDISOA <perf.alfa@mmv.madamvalue.mg>
2023-07-18 11:00:08 +0300

    Last modifications

--- candidates/serializers/certificate_serializer.py
+++ candidates/serializers/certificate_serializer.py
@@ -5,9 +5,9 @@ from candidates.models import Certificate
 class CertificateListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Certificate
-        fields = 'id', 'title', 'link', 'obtention_date'
+        fields = 'id', 'title', 'link', 'obtention_date', 'institut'
 
 class CertificatDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Certificate
-        fields = 'id', 'title', 'description', 'link', 'picture', 'obtention_date'
+        fields = 'id', 'title', 'description', 'link', 'picture', 'obtention_date', 'institut', 'candidate'
--- candidates/serializers/education_serializer.py
+++ candidates/serializers/education_serializer.py
@@ -10,7 +10,7 @@ class EducationListSerializer(serializers.ModelSerializer):
 class EducationDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Education
-        fields = 'id', 'institut', 'diplom', 'period', 'description', 'start', 'end', "candidate"
+        fields = 'id', 'institut', 'diplom', 'period', 'description', "candidate", 'start', 'end'
         extra_kwargs = {
             'start': {'write_only': True},
             'end': {'write_only': True},
