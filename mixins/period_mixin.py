bd23aa821b8176eb74a574d494684448cd3df6f6
Nomena Alfa ANDRIAMIFIDISOA <perf.alfa@mmv.madamvalue.mg>
2023-07-18 11:38:10 +0300

    Adding default now for end in PeriodMixin

--- mixins/period_mixin.py
+++ mixins/period_mixin.py
@@ -1,11 +1,11 @@
 from django.db import models
-
+from django.utils import timezone
 
 class PeriodMixin(models.Model):
     class Meta: 
         abstract = True
     
     start = models.DateField()
-    end = models.DateField()
+    end = models.DateField(default=timezone.now, blank=True)
     def period(self):
         return f"{self.start}" + (f' to {self.end}' if self.end else '')
